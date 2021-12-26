b75e584e5a1522f4ae522d5fd71bd7d73b8f4816
Rafael William <rafael.fim@hotmail.com>
2021-12-26 12:26:44 -0300

    future getData function

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
-var url = Uri.parse('https://api.hgbrasil.com/finance?format=json&key=853c4b1c');
+var url =
+    Uri.parse('https://api.hgbrasil.com/finance?format=json&key=853c4b1c');
 
 void main() async {
-
-  http.Response response = await http.get(url);
-  print(json.decode(response.body)['results']);
-  
   runApp(
     MaterialApp(
       home: Container(),
     ),
   );
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(url);
+  return json.decode(response.body);
 }
