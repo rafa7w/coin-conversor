b0ed453b35bdb1aee070bc9388ae8e0cc47681c1
Rafael William <rafael.fim@hotmail.com>
2021-12-27 19:35:58 -0300

    app interface

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,25 @@ var url =
 
 void main() async {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: const Home(),
+      theme: ThemeData(
+          hintColor: Colors.amber,
+          primaryColor: Colors.white,
+          inputDecorationTheme: (const InputDecorationTheme(
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Colors.white,
+            )),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+              color: Colors.amber,
+            )),
+            hintStyle: TextStyle(
+              color: Colors.amber,
+            ),
+          ))),
     ),
   );
 }
@@ -28,6 +44,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double? dolar;
+  double? euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,8 +85,62 @@ class _HomeState extends State<Home> {
                     ),
                   );
                 } else {
-                  return Container(
-                    color: Colors.green,
+                  dolar = snapshot.data!['results']['currencies']['USD']['buy'];
+                  euro = snapshot.data!['results']['currencies']['EUR']['buy'];
+
+                  return SingleChildScrollView(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: const [
+                        Icon(
+                          Icons.monetization_on,
+                          size: 150.0,
+                          color: Colors.amber,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: 'Reais',
+                              labelStyle: TextStyle(
+                                color: Colors.amber,
+                              ),
+                              border: OutlineInputBorder(),
+                              prefixText: 'R\$'),
+                          style: TextStyle(
+                            color: Colors.amber,
+                            fontSize: 25.0,
+                          ),
+                        ),
+                        Divider(),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: 'Dólares',
+                              labelStyle: TextStyle(
+                                color: Colors.amber,
+                              ),
+                              border: OutlineInputBorder(),
+                              prefixText: 'US\$'),
+                          style: TextStyle(
+                            color: Colors.amber,
+                            fontSize: 25.0,
+                          ),
+                        ),
+                        Divider(),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: 'Euros',
+                              labelStyle: TextStyle(
+                                color: Colors.amber,
+                              ),
+                              border: OutlineInputBorder(),
+                              prefixText: '€'),
+                          style: TextStyle(
+                            color: Colors.amber,
+                            fontSize: 25.0,
+                          ),
+                        ),
+                      ],
+                    ),
                   );
                 }
             }
