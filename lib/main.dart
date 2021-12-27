6ed3423242ce7861b5ca036f7bee776b62119495
Rafael William <rafael.fim@hotmail.com>
2021-12-27 18:26:28 -0300

    building with FutureBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,41 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.amber,
         centerTitle: true,
       ),
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return const Center(
+                  child: Text(
+                    'Carregando dados...',
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 25.0,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return const Center(
+                    child: Text(
+                      'Erro ao carregar dados :(',
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 25.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                } else {
+                  return Container(
+                    color: Colors.green,
+                  );
+                }
+            }
+          }),
     );
   }
 }
